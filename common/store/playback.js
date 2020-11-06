ed6ad5149702b04dae0edcd042a9c4fed899d4d5
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-06 01:33:08 +0100

    Allow playback property updates

--- common/store/playback.js
+++ common/store/playback.js
@@ -4,6 +4,13 @@ export default {
     mutations: {
         add(state, { name }) {
             state.push(name)
+        },
+        update_property(state, { update, name, id, own } = {}) {
+            if (own) {
+                state[id]._props[name] = update
+            } else {
+                state[id].props[name] = update
+            }
         }
     }
 }
