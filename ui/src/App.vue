43a7bffd84b8352ca8e178e1f80f3333cfceb61b
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-08 20:47:39 +0100

    Save before change to vue3

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,22 +1,33 @@
 <template>
-    <v-app>
-        <div class="row">
+    <div class="app">
+        <!--         <div class="row">
             <div class="col"><Sidebar /></div>
             <div class="col"><Selected /></div>
             <div class="col"><Properties /></div>
             <div class="col"><Tree /></div>
         </div>
-    </v-app>
+        <div class="row status">STATUS:OK</div>
+ -->
+    </div>
 </template>
 
 <style scoped>
+.app {
+    display: flex;
+    flex-direction: column;
+}
 .row {
     display: flex;
     flex-direction: row;
+    flex: 1 1 auto;
+}
+.status {
+    height: 10px;
+    flex: 0 0 auto;
 }
 .col {
     flex: 1 1;
-    border: 1px solid #58585831;
+    border: 1px solid rgb(10, 10, 10);
     border-radius: 1px;
 }
 </style>
@@ -25,24 +36,26 @@
 body {
     margin: 0;
     padding: 0;
-    background-color: #1817 !important;
+    background-color: rgb(37, 37, 37) !important;
+    font-family: "Segoe UI" !important;
 }
 </style>
 
 <script>
-import Sidebar from "./components/Sidebar";
+/* import Sidebar from "./components/Sidebar";
 import Selected from "./components/Selected";
 import Properties from "./components/Properties";
 import Tree from "./components/Tree/Tree";
-
+ */
 export default {
     name: "App",
 
     components: {
-        Sidebar,
+        /*         Sidebar,
         Selected,
         Properties,
         Tree,
+ */
     },
 
     data: () => ({}),
