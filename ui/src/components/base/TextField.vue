04a6a57deedd47e3f2a1c84e295253d0ff1ca82a
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-09 21:25:13 +0100

    Add base components

---
+++ ui/src/components/base/TextField.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <label>{{ label }}</label>
+        <input
+            type="text"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.value)"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: String,
+        label: String
+    },
+    emits: ["update:modelValue"]
+};
+</script>
+
+<style></style>
