15a68f658bbbd6a920f0a4778ce7b669603d6c04
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-09 18:51:01 +0100

    Format app component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,60 +1,68 @@
 <template>
     <div class="app__container">
-        <div class="row">
-            <!-- <div class="col"><Sidebar /></div>
-            <div class="col"><Selected /></div>
+        <div class="app__row">
+            <div class="col"><Sidebar /></div>
+            <!-- <div class="col"><Selected /></div>
             <div class="col"><Properties /></div>
             <div class="col"><Tree /></div> -->
         </div>
-        <div class="row status">STATUS:OK</div>
+        <div class="app__row app__status-bar">STATUS:OK</div>
     </div>
 </template>
 
 <style lang="scss">
+html,
+body {
+    margin: 0;
+    padding: 0;
+
+    height: 100%;
+    width: 100%;
+
+    background-color: $bg-color;
+    font-family: "Roboto";
+}
+
+#app {
+    height: 100%;
+    width: 100%;
+}
+
 .app__container {
     display: flex;
     flex-direction: column;
+    height: 100%;
+    width: 100%;
 }
 .app__row {
     display: flex;
     flex-direction: row;
     flex: 1 1 auto;
 }
-.app__ {
-    flex: 1 1;
+.app__col {
     border: 1px solid;
     border-radius: 1px;
 }
 .app__status-bar {
-    height: 10px;
+    height: 25px;
     flex: 0 0 auto;
 }
 </style>
 
-<style lang="scss">
-body {
-    margin: 0;
-    padding: 0;
-    background-color: $bg-color;
-    font-family: "Segoe UI" !important;
-}
-</style>
-
 <script>
-/* import Sidebar from "./components/Sidebar";
-import Selected from "./components/Selected";
-import Properties from "./components/Properties";
-import Tree from "./components/Tree/Tree";
- */
+import Sidebar from "./components/Sidebar";
+// import Selected from "./components/Selected";
+// import Properties from "./components/Properties";
+// import Tree from "./components/Tree/Tree";
+
 export default {
     name: "App",
 
     components: {
-        /*         Sidebar,
-        Selected,
-        Properties,
-        Tree,
- */
+        Sidebar
+        // Selected,
+        // Properties,
+        // Tree
     },
 
     data: () => ({})
