0b7265d66e67329ccd4c9efe429fafc61e3f2bf4
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-09 18:51:41 +0100

    Add Window component

---
+++ ui/src/components/Window.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="window__container">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Window"
+};
+</script>
+
+<style>
+.window__container {
+    width: 100%;
+    height: 100%;
+}
+</style>
