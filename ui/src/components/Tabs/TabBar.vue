1463dea6e6e810173ee378e8618600004659c3f6
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-09 18:51:16 +0100

    Implement custom tab component

--- ui/src/components/Tabs/TabBar.vue
+++ ui/src/components/Tabs/TabBar.vue
@@ -1,7 +1,28 @@
-<template> </template>
+<template>
+    <ul>
+        <li
+            v-for="(tab, idx) in tabs"
+            :key="idx"
+            @click="$emit('update:modelValue', idx)"
+            :class="{ 'tabs__item--active': modelValue === idx }"
+        >
+            {{ tab }}
+        </li>
+    </ul>
+</template>
 
 <script>
-export default {};
+export default {
+    props: {
+        tabs: Array,
+        modelValue: Number
+    },
+    emits: ["update:modelValue"]
+};
 </script>
 
-<style></style>
+<style>
+.tabs__item--active {
+    color: red;
+}
+</style>
---
+++ ui/src/components/Tabs/TabView.vue
@@ -0,0 +1,17 @@
+<template>
+    <component :is="tabs[modelValue]" />
+</template>
+
+<script>
+export default {
+    props: {
+        tabs: {
+            type: Array,
+            default: () => []
+        },
+        modelValue: Number
+    }
+};
+</script>
+
+<style></style>
