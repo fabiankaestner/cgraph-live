eeea3dc9d57b0dda80b169de76a3d790023f0fad
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-10 21:32:37 +0100

    Remove unnecessary properties in tree nodes

--- common/helpers/display_tree.js
+++ common/helpers/display_tree.js
@@ -1,28 +1,25 @@
-import { parseStringAddress } from "./address"
+import { parseStringAddress } from "./address";
 
 export function transformStateToDisplayTree(state, tree, parentGroup) {
     const display_tree = [];
 
     for (let node_addr of tree) {
-
-        const { type, id } = parseStringAddress(node_addr)
+        const { type, id } = parseStringAddress(node_addr);
 
         if (type === "node") {
             // we have a leaf node
 
             display_tree.push({
                 title: state.node[id].props.name.value,
-                isLeaf: true,
-                data: { address: node_addr }
+                data: { address: node_addr },
             });
-
         } else if (type === "group" && id !== parentGroup) {
             // we have a group that is different from the parent group => recursively evaluate.
             const group = state.group[id];
             display_tree.push({
                 title: group.props.name.value,
                 children: transformStateToDisplayTree(state, group.tree, id),
-                data: { address: node_addr }
+                data: { address: node_addr },
             });
         }
     }
