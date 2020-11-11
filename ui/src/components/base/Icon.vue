ad5351a03531ad79f43918b24ab6b4961e4d1d04
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-11 18:51:56 +0100

    Add icons

---
+++ ui/src/components/base/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+    <Icon :icon="icons[i]" :height="height" />
+</template>
+
+<script>
+import { Icon } from "@iconify/vue";
+import close from "@iconify-icons/mdi/close";
+
+export default {
+    name: "cg-icon",
+    props: {
+        i: String,
+        height: {
+            type: String,
+            required: false,
+            default: "24"
+        }
+    },
+    components: { Icon },
+    data() {
+        return {
+            icons: {
+                close
+            }
+        };
+    }
+};
+</script>
+
+<style>
+.icon {
+    font-family: "mdi-icons";
+}
+</style>
