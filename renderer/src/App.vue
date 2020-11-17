532c4dbe1c4bd32da12eda1cddfe6507cac69f68
Fabian Kästner <8317993+fabiankaestner@users.noreply.github.com>
2020-11-17 17:16:37 +0100

    Start the renderer project

---
+++ renderer/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <Renderer></Renderer>
+</template>
+
+<script>
+import Renderer from "./components/Renderer.vue";
+
+export default {
+    name: "App",
+    components: {
+        Renderer
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
---
+++ renderer/src/components/Renderer.vue
@@ -0,0 +1,11 @@
+<template>
+    <iframe src="./testapp.html" frameborder="0"></iframe>
+</template>
+
+<script>
+export default {
+    name: "cg-renderer"
+};
+</script>
+
+<style></style>
